e3ad71b0861cb960e57f21396044add219c7b535
Lucas carvalho dos santos <58777335+l9c4s@users.noreply.github.com>
2025-09-21 19:16:30 -0300

    feat: add NickName column to Users table and update model snapshot

---
+++ aplications/NET/Backand/Infraestructure/Migrations/20250921214515_addnickname.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnickname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NickName",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "Users");
+        }
+    }
+}
